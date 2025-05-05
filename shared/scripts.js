16eb9c7db7b89a2289731ebfebe70286e7fefc95
prSarell <55672874+prSarell@users.noreply.github.com>
2025-05-05 10:35:38 +1000

    Update scripts.js

--- shared/scripts.js
+++ shared/scripts.js
@@ -1,1 +1,11 @@
+function showAddPopup() {
+  const popup = document.getElementById('popup');
+  const title = document.getElementById('popup-title');
+  title.textContent = 'Add Category';
+  popup.style.display = 'flex';
+}
 
+function closePopup() {
+  const popup = document.getElementById('popup');
+  popup.style.display = 'none';
+}
